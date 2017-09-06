d873336723c532d19acbb21906220034774f5512
Aaron Kuzemchak <kuzzah11@gmail.com>
2017-09-05 21:27:44 -0400

    Import CSS via Webpack

--- todomvc-vue/src/main.js
+++ todomvc-vue/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
+import '../node_modules/todomvc-common/base.css'
+import '../node_modules/todomvc-app-css/index.css'
 
 new Vue({
   el: '#app',
